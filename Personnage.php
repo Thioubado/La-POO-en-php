c5715e02393f5b0803fa7c3291d33d7325dbc881
Unknown <seckndanane16@gmail.com>
2019-05-09 01:21:23 +0000

    Notre première classe : partie 1

---
+++ Personnage.php
@@ -0,0 +1,9 @@
+<?php
+class Personnage{
+    public $vie = 100;
+    public $atk = 20;
+
+    public function crier(){
+        echo 'LEEROY JENKINS';
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'Personnage.php';
+
+$merlin = new Personnage();
+var_dump($merlin->crier());
