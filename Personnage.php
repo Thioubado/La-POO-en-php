f64dc5c5d86f436b6c892bf0820aee1777c89b00
Unknown <seckndanane16@gmail.com>
2019-05-10 02:00:47 +0000

    Notre première classe : partie 2

--- Personnage.php
+++ Personnage.php
@@ -1,9 +1,31 @@
 <?php
 class Personnage{
-    public $vie = 100;
+    public $vie = 80;// je peux remplacer le 80 par le 20
     public $atk = 20;
+    public $nom;
+    
 
-    public function crier(){
-        echo 'LEEROY JENKINS';
+    public function __construct($nom){
+        $this->nom = $nom;
+    }
+
+
+    public function regenerer($vie = null){
+        if(is_null($vie)){
+            $this->vie = 100;
+        }else{
+            $this->vie += $vie;
+        }
+    }
+
+    public function mort(){
+        return $this->vie <= 0;
+    }
+
+    public function attaque($cible){
+        //this = attaquant
+        //cible = defenseur
+        // defenseur.vie -= attaquant.atk
+        $cible->vie -= $this->atk;
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 require 'Personnage.php';
 
-$merlin = new Personnage();
-var_dump($merlin->crier());
+$merlin = new Personnage("Merlin");
+$merlin->regenerer(5);
+//$merlin->nom = "Merlin";
+var_dump($merlin);
+
+echo '<br>';
+$harry = new Personnage("Harry");
+//$harry->nom = "Harry";
+//$harry->vie = -20;
+//$harry->regenerer();
+var_dump($harry);
+
+$merlin->attaque($harry);
+
+if($harry->mort()){
+    echo 'Harry est mort';
+}
+else{
+    echo 'Harry a de la chance. Il est vivant';
+}
+//var_dump($merlin->regenerer());
+
+//var_dump($harry->mort());
+
