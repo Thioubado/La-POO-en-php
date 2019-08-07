4e593fcd498a997226432d06e23914e320c5f861
Unknown <seckndanane16@gmail.com>
2019-08-07 03:07:42 +0100

    Début de création de la page article et quelques modifications

--- Blog/pages/single.php
+++ Blog/pages/single.php
@@ -1,1 +1,11 @@
-<h2>Single</h2>
+<?php
+    $post = $db->prepare('SELECT * FROM post WHERE id = ?', [$_GET['id']]);
+    
+?>
+
+
+<h2>Single qui correspond à la page des articles</h2>
+
+<h1><?= $post->titre; ?>></h1>
+
+<p><?= $post->contenu; ?></p>
--- Blog/public/index.php
+++ Blog/public/index.php
@@ -17,7 +17,7 @@ $db = new App\Database('blog');
 ob_start();
 if($p === 'home'){
     require '../pages/home.php';
-}elseif($p === 'single'){
+}elseif($p === 'article'){
     require '../pages/single.php';
 }
 $content = ob_get_clean();
