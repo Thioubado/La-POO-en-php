3bb4f38224f8a9ef92dd4481a55e8899e1a9443a
Unknown <seckndanane16@gmail.com>
2019-08-08 00:23:44 +0100

    Fin de la connexion à la bdd

--- Blog/pages/single.php
+++ Blog/pages/single.php
@@ -1,11 +1,11 @@
 <?php
-    $post = $db->prepare('SELECT * FROM post WHERE id = ?', [$_GET['id']]);
+    $post = $db->prepare('SELECT * FROM articles WHERE id = ?', [$_GET['id']], 'App\Table\Article', true);
     
 ?>
 
 
 <h2>Single qui correspond à la page des articles</h2>
 
-<h1><?= $post->titre; ?>></h1>
+<h1><?= $post->titre; ?></h1>
 
 <p><?= $post->contenu; ?></p>
